f77d3b24368850c52743db2a79dec22dc7970b85
zeeshandipro <65391484+zeeshandipro@users.noreply.github.com>
2020-11-05 21:59:53 +0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php include "index.php" ;?>
+<html>
+    <head>
+        <title>Title</title>
+    </head>
+
+    <body>
+        <h1>Login</h1>
+        <form action="" method="post">
+        <table>
+            <tr>
+                <td> Username</td>
+
+                <td><input type="text" placeholder="Username here" name="username"></td>
+            </tr>
+            <tr>
+                <td> Password</td>
+
+                <td><input type="password" placeholder="Password here" name="password"></td>
+            </tr>
+            <tr>
+
+             <td colspan="2" align="center">   <input type="submit" value="register" name="register"> </td>
+         </tr>
+
+        </table>
+
+
+        </form>
+    </body>
+</html>
